58519a5522c7a6968b54a90ab8f0d7ac5586c87e
telescopeman <80982701+telescopeman@users.noreply.github.com>
2021-09-04 15:59:18 -0400

    removing unneeded things

--- src/Alphabet.java
+++ src/Alphabet.java
@@ -17,33 +17,7 @@ public class Alphabet {
     /**
      *  The uppercase alphabet { A, B, C, ..., Z }.
      */
-
     public static final Alphabet UPPERCASE = new Alphabet("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
-
-    /**
-     *  The protein alphabet { A, C, D, E, F, G, H, I, K, L, M, N, P, Q, R, S, T, V, W, Y }.
-     */
-    public static final Alphabet PROTEIN = new Alphabet("ACDEFGHIKLMNPQRSTVWY");
-
-    /**
-     *  The base-64 alphabet (64 characters).
-     */
-    public static final Alphabet BASE64 = new Alphabet("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/");
-
-    /**
-     *  The ASCII alphabet (0-127).
-     */
-    public static final Alphabet ASCII = new Alphabet(128);
-
-    /**
-     *  The extended ASCII alphabet (0-255).
-     */
-    public static final Alphabet EXTENDED_ASCII = new Alphabet(256);
-
-    /**
-     *  The Unicode 16 alphabet (0-65,535).
-     */
-    public static final Alphabet UNICODE16      = new Alphabet(65536);
 
 
     private final char[] alphabet;     // the characters in the alphabet
@@ -77,30 +51,6 @@ public class Alphabet {
     }
 
     /**
-     * Initializes a new alphabet using characters 0 through R-1.
-     *
-     * @param radix the number of characters in the alphabet (the radix R)
-     */
-    private Alphabet(int radix) {
-        this.R = radix;
-        alphabet = new char[R];
-        inverse = new int[R];
-
-        // can't use char since R can be as big as 65,536
-        for (int i = 0; i < R; i++)
-            alphabet[i] = (char) i;
-        for (int i = 0; i < R; i++)
-            inverse[i] = i;
-    }
-
-    /**
-     * Initializes a new alphabet using characters 0 through 255.
-     */
-    public Alphabet() {
-        this(256);
-    }
-
-    /**
      * Returns true if the argument is a character in this alphabet.
      *
      * @param  c the character
@@ -109,68 +59,6 @@ public class Alphabet {
      */
     public boolean contains(char c) {
         return inverse[c] != -1;
-    }
-
-    /**
-     * Returns the number of characters in this alphabet (the radix).
-     *
-     * @return the number of characters in this alphabet
-     * @deprecated Replaced by {@link #radix()}.
-     */
-    @Deprecated
-    public int R() {
-        return R;
-    }
-
-    /**
-     * Returns the number of characters in this alphabet (the radix).
-     *
-     * @return the number of characters in this alphabet
-     */
-    public int radix() {
-        return R;
-    }
-
-    /**
-     * Returns the binary logarithm of the number of characters in this alphabet.
-     *
-     * @return the binary logarithm (rounded up) of the number of characters in this alphabet
-     */
-    public int lgR() {
-        int lgR = 0;
-        for (int t = R-1; t >= 1; t /= 2)
-            lgR++;
-        return lgR;
-    }
-
-    /**
-     * Returns the index corresponding to the argument character.
-     *
-     * @param  c the character
-     * @return the index corresponding to the character {@code c}
-     * @throws IllegalArgumentException unless {@code c} is a character in this alphabet
-     */
-    public int toIndex(char c) {
-        if (c >= inverse.length || inverse[c] == -1) {
-            throw new IllegalArgumentException("Character " + c + " not in alphabet");
-        }
-        return inverse[c];
-    }
-
-    /**
-     * Returns the indices corresponding to the argument characters.
-     *
-     * @param  s the characters
-     * @return the indices corresponding to the characters {@code s}
-     * @throws IllegalArgumentException unless every character in {@code s}
-     *         is a character in this alphabet
-     */
-    public int[] toIndices(String s) {
-        char[] source = s.toCharArray();
-        int[] target  = new int[s.length()];
-        for (int i = 0; i < source.length; i++)
-            target[i] = toIndex(source[i]);
-        return target;
     }
 
     public String toChar(int index)
@@ -200,19 +88,5 @@ public class Alphabet {
         {
             return "" + alphabet[index];
         }
-    }
-
-    /**
-     * Returns the characters corresponding to the argument indices.
-     *
-     * @param  indices the indices
-     * @return the characters corresponding to the indices {@code indices}
-     * @throws IllegalArgumentException unless {@code 0 < indices[i] < R}
-     *         for every {@code i}
-     */
-    public String toChars(int[] indices) {
-        StringBuilder s = new StringBuilder(indices.length);
-        for (int index : indices) s.append(toChar(index));
-        return s.toString();
     }
 }
